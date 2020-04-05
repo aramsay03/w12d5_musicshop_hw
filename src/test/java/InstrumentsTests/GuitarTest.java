3b9f22023ac1f63a984654bc25194063c6b65fdd
aramsay03 <alan@aramsay.co.uk>
2020-04-05 13:35:30 +0100

    First class created and 3 getter tests passing

--- src/test/java/InstrumentsTests/GuitarTest.java
+++ src/test/java/InstrumentsTests/GuitarTest.java
@@ -1,4 +1,33 @@
 package InstrumentsTests;
 
+import instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void setUp() {
+        guitar = new Guitar("Electric Bass", 4, "Varnished Oak");
+    }
+
+    @Test
+    public void canGetType() {
+        assertEquals("Electric Bass", guitar.getType());
+    }
+
+    @Test
+    public void canGetNumberOfStrings() {
+        assertEquals(4, guitar.getNumberOfStrings());
+    }
+
+    @Test
+    public void canGetMaterial() {
+        assertEquals("Varnished Oak", guitar.getMaterial());
+    }
+
 }
